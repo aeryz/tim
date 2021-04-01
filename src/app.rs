20d9e3588950de86b0d8a9926ff32f7ca777bd39
aeryz <abdullaheryz@protonmail.com>
2021-04-02 00:05:22 +0300

    refactor(app): improve code quality

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,9 @@
 use {
-    crate::{build_system, config::AppConfig, error::TimError},
+    crate::{
+        build_system::{self, BuildSystem},
+        config::AppConfig,
+        error::TimError,
+    },
     regex::Regex,
     std::{
         collections::HashSet,
@@ -7,8 +11,8 @@ use {
         fs::File,
         io::{BufRead, BufReader},
         path::{Path, PathBuf},
-        sync::{mpsc, Arc, Mutex},
         thread,
+        thread::JoinHandle,
     },
     walkdir::WalkDir,
 };
@@ -19,61 +23,52 @@ pub struct App {
 
 impl App {
     pub fn new() -> anyhow::Result<Self> {
-        Ok(App {
+        let app = App {
             config: AppConfig::parse_args()?,
-        })
+        };
+
+        if !app.config.working_dir.exists() {
+            fs::create_dir(&app.config.working_dir)?;
+        }
+
+        env::set_current_dir(&app.config.working_dir)?;
+
+        Ok(app)
     }
 
     pub fn run(mut self) -> anyhow::Result<()> {
-        if !self.config.working_dir.exists() {
-            fs::create_dir(&self.config.working_dir)?;
-        }
-
-        env::set_current_dir(&self.config.working_dir)?;
-
         self.discover_project()?;
 
-        let build_system = self.config.build_system.unwrap();
-        println!("Build system: {:?}", build_system);
-        println!("Tests: {:?}", self.config.tests);
+        let builder_thread = App::spawn_builder(
+            self.config.build_system.unwrap(),
+            self.config.project_path.clone(),
+        );
+        let parser_thread = App::spawn_parser(self.config.tests);
 
-        let test_fns = Arc::new(Mutex::new(HashSet::new()));
+        let _ = builder_thread.join().unwrap()?;
+        let test_names = parser_thread.join().unwrap()?;
 
-        let (tx, rx) = mpsc::channel();
+        println!("THREAD {:?}", test_names);
 
-        {
-            let project_path = self.config.project_path;
-            let tx_ = tx.clone();
-            thread::spawn(move || {
-                if let Err(err) = build_system.build(project_path) {
-                    tx_.send(err).unwrap();
-                }
-            });
-        }
+        Ok(())
+    }
 
-        {
-            let test_fns_ = test_fns.clone();
-            let tests = self.config.tests;
-            thread::spawn(move || {
-                for test in tests {
-                    let test_names = match Self::parse_test_names(test) {
-                        Ok(names) => names,
-                        Err(err) => {
-                            tx.send(err).unwrap();
-                            break;
-                        }
-                    };
-                    test_fns_.lock().unwrap().extend(test_names);
-                }
-            });
-        }
+    #[inline]
+    fn spawn_builder(
+        build_system: Box<dyn BuildSystem>,
+        project_path: PathBuf,
+    ) -> JoinHandle<anyhow::Result<()>> {
+        thread::spawn(move || build_system.build(project_path))
+    }
 
-        if let Ok(err) = rx.recv() {
-            Err(err)
-        } else {
-            println!("Tests: {:?}", test_fns);
-            Ok(())
-        }
+    fn spawn_parser(test_paths: HashSet<PathBuf>) -> JoinHandle<anyhow::Result<HashSet<String>>> {
+        thread::spawn(move || {
+            let mut test_names = HashSet::new();
+            for test_path in test_paths {
+                test_names.extend(App::parse_test_names(test_path)?);
+            }
+            Ok(test_names)
+        })
     }
 
     fn parse_test_names(file_path: PathBuf) -> anyhow::Result<HashSet<String>> {
@@ -106,8 +101,6 @@ impl App {
             }
         }
 
-        println!("Discover tests: {}", discover_tests);
-
         for entry in WalkDir::new(&self.config.project_path) {
             let entry = entry?;
             let metadata = entry.metadata()?;
@@ -122,7 +115,6 @@ impl App {
                     && file_name.ends_with("_test.c")
                     && !self.config.excludes.contains(Path::new(file_name))
                 {
-                    println!("?????");
                     self.config.tests.insert(entry.path().to_owned());
                 } else if found_tests != 0 && !discover_tests && file_name.ends_with("_test.c") {
                     if self.config.tests.remove(Path::new(file_name)) {
